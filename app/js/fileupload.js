fa61591bef793860042c49c76918ec81cca4960e
bdsines <bdsines007@gmail.com>
2018-07-04 18:20:47 -0500

    Added Papa Parse and Events

--- app/js/fileupload.js
+++ app/js/fileupload.js
@@ -1,4 +1,5 @@
 // (function () {
+    let Papa = require('papaparse');
     var dialog = require('electron').remote.dialog;
     var fs = require('fs');
     FileUploadBehavior = function () {
@@ -13,8 +14,12 @@
 
             var fileName = fileNames[0];
             console.log(fileName);
-            fs.readFile(fileName, 'utf-8', function (err, data) {
+            // var event = new CustomEvent('my-event', {bubbles: true, composed: true});
 
+            
+            fs.readFile(fileName, 'utf-8', function (err, data) {
+                
+                this.dispatchEvent(new CustomEvent('fileutility-file-selected',{detail: {"fileName": fileName,"filecontent":data}}));
                 // document.getElementById("editor").value = data;
                 console.log("data",data);
 
@@ -22,6 +27,15 @@
 
         });
 
+    }
+    FileUploadBehavior.parseFile =function(fileName,fileObject){
+        Papa.parse(fileObject,{
+            header:true,
+            complete:function(results){
+                console.log("Papa Parsed It!",results);
+                window.dispatchEvent(new CustomEvent('fileutility-file-parsed', {detail: {"fileName": fileName,"filecontent":results.data,"errors":results.errors}}));
+            }
+        })
     }
 // });
 
