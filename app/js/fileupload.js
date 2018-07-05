d9c01a0485d69b8e6e7683eb073b733a6ae3aa33
bdsines <bdsines007@gmail.com>
2018-07-05 08:22:12 -0500

    added file filter and dom-repeat

--- app/js/fileupload.js
+++ app/js/fileupload.js
@@ -1,42 +1,48 @@
 // (function () {
-    let Papa = require('papaparse');
-    var dialog = require('electron').remote.dialog;
-    var fs = require('fs');
-    FileUploadBehavior = function () {
+let Papa = require('papaparse');
+var dialog = require('electron').remote.dialog;
+var fs = require('fs');
+FileUploadBehavior = function () {
 
-    };
+};
 
-    FileUploadBehavior.openFile = function () {
+FileUploadBehavior.openFile = function () {
 
-        dialog.showOpenDialog(function (fileNames) {
+    dialog.showOpenDialog({
+        filters: [
 
-            if (fileNames === undefined) return;
+            { name: 'text', extensions: ['txt'] }, { name: 'csv', extensions: ['csv'] }, { name: 'dat', extensions: ['dat'] }
 
-            var fileName = fileNames[0];
-            console.log(fileName);
-            // var event = new CustomEvent('my-event', {bubbles: true, composed: true});
+        ]
+    }, function (fileNames) {
 
-            
-            fs.readFile(fileName, 'utf-8', function (err, data) {
-                
-                this.dispatchEvent(new CustomEvent('fileutility-file-selected',{detail: {"fileName": fileName,"filecontent":data}}));
-                // document.getElementById("editor").value = data;
-                console.log("data",data);
+        if (fileNames === undefined) return;
 
-            });
+        var fileName = fileNames[0];
+        console.log(fileName);
+        // var event = new CustomEvent('my-event', {bubbles: true, composed: true});
+
+
+        fs.readFile(fileName, 'utf-8', function (err, data) {
+
+            window.dispatchEvent(new CustomEvent('fileutility-file-selected', { detail: { "fileName": fileName, "filecontent": data } }));
+            // document.getElementById("editor").value = data;
+            console.log("data", data);
 
         });
 
-    }
-    FileUploadBehavior.parseFile =function(fileName,fileObject){
-        Papa.parse(fileObject,{
-            header:true,
-            complete:function(results){
-                console.log("Papa Parsed It!",results);
-                window.dispatchEvent(new CustomEvent('fileutility-file-parsed', {detail: {"fileName": fileName,"filecontent":results.data,"errors":results.errors}}));
-            }
-        })
-    }
+    });
+
+}
+FileUploadBehavior.parseFile = function (fileName, fileObject) {
+    Papa.parse(fileObject, {
+        header: true,
+        complete: function (results) {
+            console.log("Papa Parsed It!", results);
+            window.dispatchEvent(new CustomEvent('fileutility-file-parsed', { detail: { "fileName": fileName, "filecontent": results.data, "errors": results.errors } }));
+        }
+    })
+}
 // });
 
 
